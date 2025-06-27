ef8977707f77feb03481c85bfd2ab1851e247702
Shannon Taylor <shannon.elisa.taylor@gmail.com>
2025-06-27 15:43:33 +1200

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -4,7 +4,7 @@ const enNavbarData = {
   Home: "Home",
   publications: "Publications",
   Research: "Research",
-  Jobs: "Jobs",
+  Jobs: "Education",
   Contact: "Contact",
 };
 
@@ -31,7 +31,7 @@ const enPublicationsPageData = {
       date: "2024",
       link: "https://www.cell.com/iscience/fulltext/S2589-0042(24)02065-0",
       download: "https://www.cell.com/action/showPdf?pii=S2589-0042%2824%2902065-0",
-      writers: ["Kay Speiss", "Shannon E. Taylor", "Timothy Fulton", "Kane Toh", "Dillan Saunders", "Seongwon Huang", "Yuxan Wang", "Brooks Paige", "Benjamin Steventon", "Berta Verd"
+      writers: ["Kay Speiss", "<b>Shannon E. Taylor</b> (co-first author)", "Timothy Fulton", "Kane Toh", "Dillan Saunders", "Seongwon Huang", "Yuxan Wang", "Brooks Paige", "Benjamin Steventon", "Berta Verd"
       ],
     },
     {
@@ -40,13 +40,13 @@ const enPublicationsPageData = {
       date: "2022",
       link: "https://journals.biologists.com/dev/article/149/5/dev199632/274657/The-Nasonia-pair-rule-gene-regulatory-network",
       download: "https://journals.biologists.com/dev/article-pdf/149/5/dev199632/2135760/dev199632.pdf", 
-      writers: ["Shannon E. Taylor", "Peter K. Dearden"],
+      writers: ["<b>Shannon E. Taylor</b>", "Peter K. Dearden"],
     },
     {
       title: `The torso-like gene functions to maintain the structure of the vitelline membrane in Nasonia vitripennis, implying its co-option into Drosophila axis formation`, 
       abstract: `Axis specification is a fundamental developmental process. Despite this, the mechanisms by which it is controlled across insect taxa are strikingly different. An excellent example of this is terminal patterning, which in Diptera such as Drosophila melanogaster occurs via the localized activation of the receptor tyrosine kinase Torso. In Hymenoptera, however, the same process appears to be achieved via localized mRNA. How these mechanisms evolved and what they evolved from remains largely unexplored. Here, we show that torso-like, known for its role in Drosophila terminal patterning, is instead required for the integrity of the vitelline membrane in the hymenopteran wasp Nasonia vitripennis. We find that other genes known to be involved in Drosophila terminal patterning, such as torso and Ptth, also do not function in Nasonia embryonic development. These findings extended to orthologues of Drosophila vitelline membrane proteins known to play a role in localizing Torso-like in Drosophila; in Nasonia these are instead required for dorso–ventral patterning, gastrulation and potentially terminal patterning. Our data underscore the importance of the vitelline membrane in insect development, and implies phenotypes caused by knockdown of torso-like must be interpreted in light of its function in the vitelline membrane. In addition, our data imply that the signalling components of the Drosophila terminal patterning systems were co-opted from roles in regulating moulting, and co-option into terminal patterning involved the evolution of a novel interaction with the vitelline membrane protein Torso-like.`, 
       date: `2019`, 
-      writers: ["Shannon E. Taylor", "Jack Tuffery", "Daniel Bakopoulos", "Sharon Lequeux", "Coral G. Warr", "Travis K. Johnson", "Peter K. Dearden"]
+      writers: ["<b>Shannon E. Taylor</b>", "Jack Tuffery", "Daniel Bakopoulos", "Sharon Lequeux", "Coral G. Warr", "Travis K. Johnson", "Peter K. Dearden"]
     },
   ],
 
@@ -57,26 +57,26 @@ const enPublicationsPageData = {
       abstract: `Modified, agricultural landscapes are susceptible to damage by insect pests. Biological control of pests is typically successful once a control agent has established, but this depends on the agent’s capacity to co-evolve with the host. Theoretical studies have shown that different levels of genetic variation between the host and the control agent will lead to rapid evolution of resistance in the host. Although this has been reported in one instance, the underlying genetics have not been studied. To address this, we measured the genetic variation in New Zealand populations of the pasture pest, Argentine stem weevil (Listronotus bonariensis), which is controlled with declining effectiveness by a parasitoid wasp, Microctonus hyperodae. We constructed a draft reference genome of the weevil, collected samples from a geographical survey of 10 sites around New Zealand, and genotyped them using a modified genotyping-by-sequencing approach. New Zealand populations of Argentine stem weevil have high levels of heterozygosity and low population structure, consistent with a large effective population size and frequent gene flow. This implies that Argentine stem weevils were able to evolve more rapidly than their biocontrol agent, which reproduces asexually. These findings show that monitoring genetic diversity in biocontrol agents and their targets is critical for long-term success of biological control.`,
       date: "2020",
       link: "https://www.mdpi.com/2075-4450/11/7/441",
-      writers: ["Thomas W. R. Harrop 1", "Marissa F. Le Lec", "Ruy Jauregui", "Shannon E. Taylor", "Sarah N. Inwood", "Tracey van Stijn", "Hannah Henry", "John Skelly", "Siva Ganesh", "Rachael L. Ashby", "Jeanne M. E. Jacobs", "Stephen L. Goldson", "Peter K. Dearden"],
+      writers: ["Thomas W. R. Harrop 1", "Marissa F. Le Lec", "Ruy Jauregui", "<b>Shannon E. Taylor</b>", "Sarah N. Inwood", "Tracey van Stijn", "Hannah Henry", "John Skelly", "Siva Ganesh", "Rachael L. Ashby", "Jeanne M. E. Jacobs", "Stephen L. Goldson", "Peter K. Dearden"],
     },
     {
       title: `When ‘#xmasangels’ Tweet: a Reception Study of Craftivism as Christian Witness `,
       abstract: `Craftivism combines craft and activism. This paper contributes to the field of contemporary culture, mission, and creative making by examining the Christmas Angels project, in which local churches yarn-bomb hand-knitted angels, as an ecclesial expression of craftivism. Recipient responses to this fresh expression of Christian witness are analysed by examination of over 1,100 ‘#xmasangel’ tweets. Analysis reveals a found theology, in which angels are received with joy and surprise, understood in the context of love, experienced as a place-based gift and embody a participative making. A missiology of making is developed, reading the yarn-bombed Christmas angels as an ecclesial practice of witness in continuity with a theology of making in the Wisdom literature and ‘craft-egesis’ of mission in Acts. The research has relevance in exploring the potential of digital data in empirical ecclesial research and challenging missiology to be practical in ‘making’ a domestic turn.`,
       date: "2020",
       link: "https://brill.com/view/journals/ep/7/2/article-p143_143.xml",
-      writers: ["Steve Taylor", "Shannon E. Taylor"],
+      writers: ["Steve Taylor", "<b>Shannon E. Taylor</b>"],
     },
         {
       title: `The honeybee as a model insect for developmental genetics`,
       abstract: `Honeybees are an important component of modern agricultural systems, and a fascinating and scientifically engrossing insect. Honeybees are not commonly used as model systems for understanding development in insects despite their importance in agriculture. Honeybee embryogenesis, while being superficially similar to Drosophila, is molecularly very different, especially in axis formation and sex determination. In later development, much of honeybee biology is modified by caste development, an as yet poorly understood, but excellent, system to study developmental plasticity. In adult stages, developmental plasticity of the ovaries, related to reproductive constraint exhibits another aspect of plasticity. Here they review the tools, current knowledge and opportunities in honeybee developmental biology, and provide an updated embryonic staging scheme to support future studies.`,
       date: "2017",
       link: "https://onlinelibrary.wiley.com/doi/abs/10.1002/dvg.23019",
-      writers: ["AG Cridge", "MR Lovegrove", "JG Skelly", "SE Taylor", "GEL Petersen", "RC Cameron", "PK Dearden"],
+      writers: ["AG Cridge", "MR Lovegrove", "JG Skelly", "<b>SE Taylor</b>", "GEL Petersen", "RC Cameron", "PK Dearden"],
     },
 
   ],
 
-  type_three_title: "Under Review",
+  type_three_title: "",//Under Review
   type_three_items: [
 
   ],
