e1426ad63ccce7c3113cb5a549cf879b9bfbd8bf
Shannon Taylor <shannon.elisa.taylor@gmail.com>
2025-06-27 13:03:23 +1200

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -2,7 +2,7 @@
 const enNavbarData = {
   title: " Your name ",
   Home: "Home",
-  publications: "publications",
+  publications: "Publications",
   Research: "Research",
   Jobs: "Jobs",
   Contact: "Contact",
@@ -10,22 +10,12 @@ const enNavbarData = {
 
 // ---------- Home page data
 const enHomePageData = {
-  name: " Your name ",
+  name: " Shannon Taylor ",
   jobTitle: " Job title ",
   home_title: "About me",
   home_content: `
   <div>
-    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-    <h2 class='title'> Main works </h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh  </p>
-    <ul>
-      <li> Heuristics</li>
-      <li> Policy planning </li>
-      <li> Flexiblity to details </li>
-    </ul>
+  <p> I am passionate about... </p> 
   </div>
   `,
 };
